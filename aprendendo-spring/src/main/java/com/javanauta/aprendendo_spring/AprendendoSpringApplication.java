0117dcd62951589918e1f2e018263ab674ecae1a
calebewerneckcouto <calebewerneck@hotmail.com>
2026-08-24 15:06:43 -0300

    testando a aplicacao

--- aprendendo-spring/src/main/java/com/javanauta/aprendendo_spring/AprendendoSpringApplication.java
+++ aprendendo-spring/src/main/java/com/javanauta/aprendendo_spring/AprendendoSpringApplication.java
@@ -8,6 +8,12 @@ public class AprendendoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AprendendoSpringApplication.class, args);
+
+		Usuario joao = new Usuario("Joao", "joaodasilva@hotmail.com", "123156");
+		joao.setNome("João Silva");
+		System.out.println(joao.getNome());
+		joao.setNome("Joao da Silva");
+		System.out.println(joao.getEmail());
 	}
 
 }
